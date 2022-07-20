6eac56373a9fd32a13459217b6212d9df5026474
Stark Rajput <82673462+starkrajput@users.noreply.github.com>
2022-07-20 13:34:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+// console.log("hi")
+// function func(){
+//     var x = document.getElementById("num").value;
+//     document.getElementById("para").style.fontSize = "90px";
+//     document.getElementById("para").style.color = "red";
+//     document.getElementById("para").innerHTML="Hello" + x;
+//     document.getElementById("num").value = "";
+//     alert("Hellow world" + x);
+ 
+// }
+
+// function table(){
+//     var n = document.getElementById('num').value;
+//     var arr = [];
+//     for(var i =1;i<=10;i++){
+//        arr.push(n + "x" + i +"=" +n*i);
+//     }
+//     document.getElementById("para").innerHTML = arr;
+// }
+
+// function numc(){
+//   var x = document.getElementById("num");
+//   if(num%2 == 0){
+//     document.getElementById('para').innerHTML = "Even";
+//   }
+//   else{
+//     document.getElementById('para').innerHTML = "Odd";
+//   }
+// }
+
+function brazilFunc(){
+    var c = "This is Brazil Flag";
+    document.getElementById("brazil").innerHTML = c;
+}
+
+function outBrazil(){
+    var c = ""
+    document.getElementById("brazil").innerHTML = c;
+}
+
+
+function indiaFunc(){
+    var c = "This is India Flag";
+    document.getElementById("india").innerHTML = c;
+}
+
+function outindia(){
+    var c = ""
+    document.getElementById("india").innerHTML = c;
+}
+
+
+
+function usFunc(){
+    var c = "This is US Flag";
+    document.getElementById("us").innerHTML = c;
+}
+
+function outus(){
+    var c = ""
+    document.getElementById("us").innerHTML = c;
+}
+
+
+
+function britainFunc(){
+    var c = "This is Britain Flag";
+   
+    document.getElementById("britain").innerHTML = c ;
+}
+
+function outBritain(){
+    var c = ""
+    document.getElementById("britain").innerHTML = c;
+}
+
+
+
+
+function rusFunc(){
+    var c = "This is Russia Flag";
+    document.getElementById("russia").innerHTML = c;
+}
+
+function outrus(){
+    var c = ""
+    document.getElementById("russia").innerHTML = c;
+}
+
+
+
+function fraFunc(){
+    var c = "This is France Flag";
+    document.getElementById("france").innerHTML = c;
+}
+
+function outfra(){
+    var c = ""
+    document.getElementById("france").innerHTML = c;
+}
